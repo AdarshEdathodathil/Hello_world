e443fc485dbbe1d35107c237425f9c709295c2b5
adarsh <adarshedathodathil@gmail.com>
2024-06-20 14:12:55 +0530

    final commit

---
+++ bin/function_application.dart
@@ -0,0 +1,5 @@
+import 'package:function_application/function_application.dart' as function_application;
+
+void main() {
+ print("Hello world");
+}
---
+++ test/function_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:function_application/function_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
